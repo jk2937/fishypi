4b8798d656f47ab271141bf68f4037a8dfc5e2ab
jk2937 <kaschakjonathan+github@gmail.com>
2024-06-04 09:59:01 -0400

    Update midi documentation

--- python-midi/midi-through-port-test.py
+++ python-midi/midi-through-port-test.py
@@ -2,7 +2,16 @@ import pygame.midi
 
 pygame.midi.init()
 
-device = pygame.midi.Input(1)
+device_id = 5
+
+global device_count
+device_count = pygame.midi.get_count()
+for i in range(device_count):
+    device_info = pygame.midi.get_device_info(i)
+    if device_info[2] == 1 and b"Midi Through" in device_info[1]:
+        device_id = i
+
+device = pygame.midi.Input(device_id)
 
 while True:
     if device.poll():
